23db947144aa830cd513acb426941b362f7486f6
VighneshManjrekar <69366251+VighneshManjrekar@users.noreply.github.com>
2020-10-20 17:18:35 +0530

    make it better1

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-var socket = io.connect('https://arcade-games-mix-chatbox.herokuapp.com/')
+var socket = io.connect('https://arcade-games-mix.herokuapp.com/')
 
 var message = document.getElementById('msg-box')
 var data = document.querySelector('.msg')
