89215b36f55bdd6b79715fcf938a1eb119323026
VighneshManjrekar <69366251+VighneshManjrekar@users.noreply.github.com>
2021-04-28 19:36:31 +0530

    My first commit

--- public/index.js
+++ public/index.js
@@ -1,10 +1,8 @@
 window.addEventListener("DOMContentLoaded", () => {
     var socket = io.connect('https://arcade-games-mix.herokuapp.com/')
     document.addEventListener('contextmenu', event => event.preventDefault())
-
     var message = document.getElementById('msg-box')
     var data = document.querySelector('.msg')
-
     const append = (m, p) => {
         var msg = document.createElement('div')
         msg.innerHTML = m
@@ -12,29 +10,35 @@ window.addEventListener("DOMContentLoaded", () => {
         msg.classList.add(p)
         data.append(msg)
     }
-
     var name = prompt('Enter Your name to join the chat : ')
-    socket.emit('new-user', name)
-    socket.on('user-joined', name => {
-        append(`${name} joined the chat`, 'center')
-    })
-    socket.on('recieve', msgs => {
-        append(`${msgs.name} : ${msgs.message}`, 'rec')
-    })
-
-    function send() {
-        const mesg = message.value
-        append(`You : ${mesg}`, 'send')
-        socket.emit('send', mesg)
-        message.value = ''
-    }
-
-    message.addEventListener('keypress', function (event) {
-        if (event.keyCode === 13) {
-            event.preventDefault()
-            send()
+    if (name == null || name == "") {
+        while (name == null || name == "") {
+            name = prompt("Enter your name : ")
         }
-    })
-
-    document.getElementById("send-btn").addEventListener("click",send)
+        execute()
+    } else {
+        execute()
+    }
+    function execute() {
+        document.getElementById("send-btn").addEventListener("click", send)
+        socket.emit('new-user', name)
+        socket.on('user-joined', name => {
+            append(`${name} joined the chat`, 'center')
+        })
+        socket.on('recieve', msgs => {
+            append(`${msgs.name} : ${msgs.message}`, 'rec')
+        })
+        function send() {
+            const mesg = message.value
+            append(`You : ${mesg}`, 'send')
+            socket.emit('send', mesg)
+            message.value = ''
+        }
+        message.addEventListener('keypress', function (event) {
+            if (event.keyCode === 13) {
+                event.preventDefault()
+                send()
+            }
+        })
+    }
 })
